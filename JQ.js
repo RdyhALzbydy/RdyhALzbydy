f36636a9eedd1872d76003d1816d3f2a1d36d78d
RdyhALzbydy <s438006630@st.uqu.edu.sa>
2021-09-22 04:58:16 +0300

    Project

---
+++ JQ.js
@@ -0,0 +1,13 @@
+$('.flag').hover(function() {
+    $(this).css("cursor", "pointer");
+    $(this).animate({
+        width: "50%",
+        height: "50%"
+    }, 'slow');
+
+}, function() {
+    $(this).animate({
+        width: "28%"
+    }, 'slow');
+
+});
